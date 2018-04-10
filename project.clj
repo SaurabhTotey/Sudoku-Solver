24fa9bff4c5d9787f7e3e3f757956af18a542369
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-09 19:46:18 -0600

    Made Hello World

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject sudoku-solver "0.1.0-SNAPSHOT"
   :description "A project to solve a given Sudoku Board"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main sudoku-solver.core)
