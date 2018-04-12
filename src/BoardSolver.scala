0f77368dc1a55ca1a688fe78336f1b209ec9b43c
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-12 17:15:10 -0600

    Shortened/clarified some of board solving code

--- src/BoardSolver.scala
+++ src/BoardSolver.scala
@@ -11,12 +11,12 @@ def solveBoard(initialBoard: Board): Array[Board] = {
     var backtrackVal = 0
     var backtrackR = -1
     var backtrackC = -1
-    var currentBoard: Board = if ((0 until 9).map(i => 9 - initialBoard.row(i).filterNot(num => num == 0).length).contains(1)) {
-        val rowIndex = (0 until 9).filter(i => 9 - initialBoard.row(i).filterNot(num => num == 0).length == 1)(0)
+    var currentBoard: Board = if ((0 until 9).exists(i => initialBoard.row(i).count(num => num == 0) == 1)) {
+        val rowIndex = (0 until 9).filter(i => initialBoard.row(i).count(num => num == 0) == 1)(0)
         val columnIndex = (0 until 9).filter(i => initialBoard.value(rowIndex, i) == 0)(0)
         initialBoard.alterValue(rowIndex, columnIndex, (1 to 9).filterNot(num => initialBoard.row(rowIndex).contains(num))(0))
-    } else if ((0 until 9).map(i => 9 - initialBoard.column(i).filterNot(num => num == 0).length).contains(1)) {
-        val columnIndex = (0 until 9).filter(i => 9 - initialBoard.column(i).filterNot(num => num == 0).length == 1)(0)
+    } else if ((0 until 9).exists(i => initialBoard.column(i).count(num => num == 0) == 1)) {
+        val columnIndex = (0 until 9).filter(i => initialBoard.column(i).count(num => num == 0) == 1)(0)
         val rowIndex = (0 until 9).filter(i => initialBoard.value(i, columnIndex) == 0)(0)
         initialBoard.alterValue(rowIndex, columnIndex, (1 to 9).filterNot(num => initialBoard.column(columnIndex).contains(num))(0))
     } else {
