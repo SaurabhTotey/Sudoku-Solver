2456aa4179ecf5b3713e5fb8d8c6fa4ecace1541
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-12 23:13:31 -0600

    Made start to new solveBoard function

--- src/BoardSolver.scala
+++ src/BoardSolver.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * A namespace to separate out board solving logic from other parts or sections of the code
   */
@@ -12,6 +14,24 @@ object BoardSolver {
     def solveBoard(initialBoard: Board): Array[Board] = {
         if (initialBoard.isFilled) {
             return Array(initialBoard)
+        }
+        val possibleSolutions = initialBoard.values.map(row => row.map(_ => new ArrayBuffer[Int]()))
+        for ((row, i) <- possibleSolutions.zipWithIndex) {
+            for ((list, j) <- row.zipWithIndex) {
+                for (value <- (0 until 9).filter(possibleValue => initialBoard.alterValue(i, j, possibleValue).isValid)) {
+                    list.append(value)
+                }
+                if (list.length == 1) {
+                    return Array(initialBoard) ++ solveBoard(initialBoard.alterValue(i, j, list(0)))
+                } else {
+                    for (possibleAnswer <- list) {
+                        val branchedSolution = solveBoard(initialBoard.alterValue(i, j, possibleAnswer))
+                        if (branchedSolution(branchedSolution.length - 1).isValid) {
+                            return Array(initialBoard) ++ branchedSolution
+                        }
+                    }
+                }
+            }
         }
         null
     }
