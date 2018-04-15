453a78a2d2246a3dc3650169f78dc27c65dd4af7
Saurabh Totey <SaurabhTotey@gmail.com>
2018-04-14 23:30:13 -0600

    Made solution algorithm that doesn't stack overflow, but runs forever

--- src/BoardSolver.scala
+++ src/BoardSolver.scala
@@ -11,29 +11,24 @@ object BoardSolver {
       */
     def solveBoard(initialBoard: Board): Array[Board] = {
         def getBoardSquarePossibilities(board: Board): Seq[Seq[Seq[Int]]] = (0 until 9).map(i => (0 until 9).map(j => (0 until 9).filter(possibleValue => board.alterValue(i, j, possibleValue).isValid)))
-        def innerSolveBoard(initialBoard: Board, possibleSolutions: Seq[Seq[Seq[Int]]]): Array[Board] = {
-            if (!initialBoard.isValid || possibleSolutions.isEmpty) {
-                throw new Exception()
-            }
-            if (initialBoard.isFilled) {
+        def innerSolveBoard(initialBoard: Board, possibleSolutions: Seq[Seq[Seq[Int]]], fallbackBoard: Board = null, fallbackPossibleSolutions: Seq[Seq[Seq[Int]]] = null): Array[Board] = {
+            if (initialBoard.isFilled && initialBoard.isValid) {
                 return Array(initialBoard)
             }
-            val squareLeastSolutions = (0 until 81).map(i => Array(i / 9, i % 9)).sortWith((a, b) => possibleSolutions(a(0))(a(1)).length < possibleSolutions(b(0))(b(1)).length).head
+            if (!initialBoard.isValid || possibleSolutions.isEmpty) {
+                return innerSolveBoard(fallbackBoard, fallbackPossibleSolutions)
+            }
+            val squareLeastSolutions = (0 until 81).map(i => Array(i / 9, i % 9)).minBy(a => possibleSolutions(a(0))(a(1)).length)
             val nextAnswerForSquare = possibleSolutions(squareLeastSolutions(0))(squareLeastSolutions(1)).head
             val nextBoard = initialBoard.alterValue(squareLeastSolutions(0), squareLeastSolutions(1), nextAnswerForSquare)
-            try {
-                innerSolveBoard(nextBoard, getBoardSquarePossibilities(nextBoard))
-            } catch {
-                case _: Exception =>
-                    val newPossibleSolutions = (0 until 9).map(i => (0 until 9).map(j =>
-                        if (i == squareLeastSolutions(0) && j == squareLeastSolutions(1)) {
-                            possibleSolutions(i)(j).filter(element => element != nextAnswerForSquare)
-                        } else {
-                            possibleSolutions(i)(j)
-                        }
-                    ))
-                    innerSolveBoard(initialBoard, newPossibleSolutions)
-            }
+            val fallback = (0 until 9).map(i => (0 until 9).map(j =>
+                if (i == squareLeastSolutions(0) && j == squareLeastSolutions(1)) {
+                    possibleSolutions(i)(j).filter(element => element != nextAnswerForSquare)
+                } else {
+                    possibleSolutions(i)(j)
+                }
+            ))
+            innerSolveBoard(nextBoard, getBoardSquarePossibilities(nextBoard), initialBoard, fallback)
         }
         innerSolveBoard(initialBoard, getBoardSquarePossibilities(initialBoard))
     }
